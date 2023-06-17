a4be84e2b3106e392d8694a415430c39efae83ff
isaacwuerth <isaac.wuerth@outlook.com>
2023-06-17 19:52:23 +0200

    Remove all res

--- main.tf
+++ main.tf
@@ -51,26 +51,3 @@ resource "ssh_resource" "cloud_init_vendor" {
     permissions = "0644"
   }
 }
-
-module "webtools-itsvc-ch" {
-  count = 4
-  source = "./modules/vm"
-  cloudflare_zone_id = var.cloudflare_zone_id
-  name = "k8s-host-${count.index}.itsvc.ch"
-  cores = 4
-  sockets = 1
-  memory = 4096
-  disk_size = "100G"
-  ipv4addr = "10.0.10.11${count.index}"
-  ipv4gw = "10.0.10.1"
-  ipv4mask = "24"
-  network_bridge = "vmbr0"
-  username = "itsvcadmin"
-  sshkeys = <<-EOT
-    ${var.ssh_key_public_mgmt}
-    ${var.ssh_key_public_admin}
-  EOT
-  ssh_key_public_mgmt = var.ssh_key_public_mgmt
-  ssh_key_private_mgmt = var.ssh_key_private_mgmt
-  ansible_file = "./ansible/prov.yml"
-}
